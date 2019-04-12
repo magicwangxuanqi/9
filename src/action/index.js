6fc8c194f50eee4791e3762ae75daaab37110ff0
magicwangxuanqi <1794594886@qq.com>
2019-04-12 10:42:50 +0800

    remove files

--- src/action/index.js
+++
@@ -1,8 +0,0 @@
-export const login = loginFlag => ({
-    type: 'LOGIN',
-    loginFlag
-})
-export const register = registerFlag => ({
-    type: 'REGISTER',
-    registerFlag
-})
--- src/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import UserStatusReducer from "./user.reducer";
-
-export default combineReducers({
-  UserStatusReducer
-});
